35bbb7aa9f1b5f74e75e98f3c67f6000cb3506ff
Florian Glashauser <florian.glashauser@pc-giga.de>
2024-02-13 17:19:45 +0000

    Rebuild frontend

---
+++ contracts/contracts/doctype/contract_settings/contract_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, PC-Giga and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Contract Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
